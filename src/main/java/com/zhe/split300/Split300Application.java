72b70ec7ea219d299df22ed091bb41460ef8c7d7
Konstantin_Zhe <konstantin4ik33@gmail.com>
2024-04-12 20:06:24 +0300

    add SpringBootInit

---
+++ src/main/java/com/zhe/split300/Split300Application.java
@@ -0,0 +1,13 @@
+package com.zhe.split300;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Split300Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Split300Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhe/split300/Split300ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhe.split300;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Split300ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
