d4fbc9ec4d06ceb2ec5f4c8c056672fb1130eab7
Konstantin_Zhe <konstantin4ik33@gmail.com>
2024-04-15 14:35:54 +0300

    add create new person and index

--- src/main/java/com/zhe/split300/controllers/PersonController.java
+++ src/main/java/com/zhe/split300/controllers/PersonController.java
@@ -1,10 +1,16 @@
 package com.zhe.split300.controllers;
 
-import com.zhe.split300.repositories.PersonRepository;
+import com.zhe.split300.models.Person;
+
 import com.zhe.split300.services.PersonService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +24,21 @@ public class PersonController {
         this.personService = personService;
     }
 
-    @GetMapping
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("person", personService.findAll());
+        return "person/index";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "person/new";
+    }
+    @PostMapping()
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "person/new";
+        return "redirect:/person";
+    }
 }
