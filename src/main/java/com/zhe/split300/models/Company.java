ed16cb3be627aef5b1233589924d89f88259903d
Konstantin_Zhe <konstantin4ik33@gmail.com>
2024-04-17 13:22:42 +0300

    add Company Entity

---
+++ src/main/java/com/zhe/split300/models/Company.java
@@ -0,0 +1,50 @@
+package com.zhe.split300.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@ToString
+@Table(name = "Company")
+public class Company {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 2, max = 50, message = "Имя должно быть от 2 до 50 символов.")
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "person_company",
+            joinColumns = @JoinColumn(name = "company_id"),
+            inverseJoinColumns = @JoinColumn(name = "person_id"))
+    @ToString.Exclude
+    private List<Person> persons;
+}
