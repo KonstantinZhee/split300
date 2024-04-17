e8404b1acf3e62d3624f0a005ed6b2ae81ae667e
Konstantin_Zhe <konstantin4ik33@gmail.com>
2024-04-17 20:58:31 +0300

    add CompanyRepository

---
+++ src/main/java/com/zhe/split300/repositories/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.zhe.split300.repositories;
+
+import com.zhe.split300.models.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
+}
