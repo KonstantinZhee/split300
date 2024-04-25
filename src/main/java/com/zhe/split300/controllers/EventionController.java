febcc1fabf8366d243ba1df232e6f8e8fc4ee76c
Konstantin_Zhe <konstantin4ik33@gmail.com>
2024-04-25 20:40:59 +0300

    add Eventions READ ALL

--- src/main/java/com/zhe/split300/controllers/EventionController.java
+++ src/main/java/com/zhe/split300/controllers/EventionController.java
@@ -1,10 +1,16 @@
 package com.zhe.split300.controllers;
 
+import com.zhe.split300.models.Company;
+import com.zhe.split300.models.Evention;
 import com.zhe.split300.services.EventionService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,5 +27,15 @@ public class EventionController {
     public String index(Model model) {
         model.addAttribute("evention", eventionService.findAll());
         return "eventions/index";
+    }
+    @PostMapping()
+    //Создаем одну новую запись (2)
+    public String create(@ModelAttribute("evention") @Valid Evention evention, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "eventions/new";
+        } else {
+            eventionService.save(evention);
+        }
+        return "redirect:/eventions";
     }
 }
