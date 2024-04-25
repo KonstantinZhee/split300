6e3881de3172c55bef8bcd700de5c0a26a1afcb4
Konstantin_Zhe <konstantin4ik33@gmail.com>
2024-04-25 16:09:24 +0300

    add All Entities

--- src/main/java/com/zhe/split300/models/PayedFor.java
+++ src/main/java/com/zhe/split300/models/PayedFor.java
@@ -8,62 +8,40 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
-import java.util.Date;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Setter
-@Table(name = "Operation")
+@Table(name = "payed_for")
 @ToString
-public class Operation {
+public class PayedFor {
 
     @Id
     @Column(name = "uid")
-    @NotNull
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID uid;
 
     @ManyToOne
-    @JoinColumn(name = "evention_uid", referencedColumnName = "uid")
-    private UUID eventionUUID;
-
-    @NotBlank(message = "Поле не должно быть пустым.")
-    @Column(name = "name")
-    private String name;
+    @JoinColumn(name = "operation_uid", referencedColumnName = "uid")
+    private UUID operationUUID;
 
     @Column(name = "value")
     @NotBlank(message = "Укажите сумму.")
     @Digits(message = "Укажите цифровое значение по второй знак после запятой например: 36,28", integer = 100, fraction = 2)
     private BigDecimal value;
 
-    @Column(name = "time")
-    @Temporal(TemporalType.TIMESTAMP)
-    @DateTimeFormat(pattern = "dd/MMMM/yyyy - HH:mm:ss")
-    private Date time;
-
-    @Column(name = "note")
-    @Size(min = 5, max = 100, message = "Комментарий должен быть от 5 до 100 символов.")
-    private String note;
-
-    @OneToMany(mappedBy = "eventionUUID")
-    @ToString.Exclude
-    private List<Operation> operations;
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    private Person person;
 }
